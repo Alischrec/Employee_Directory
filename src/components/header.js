a00640ead06e564c3e6f441769588d1832b500bb
Alischrec <alischreck@gmail.com>
2021-02-06 22:19:04 -0800

    changed app name and description

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ function Header(props) {
         <div className="jumbotron jumbotron-fluid">
             <div className="container">
                 <h1 className="display-4">Employee Directory</h1>
-                <p className="lead">Click on carrots to filter by heading or use the search box to narrow your results.</p>
+                <p className="lead">Click on column name to filter by heading or use the search box to narrow your results.</p>
             <hr className='my-4 underline'></hr>
             </div>
         </div>
